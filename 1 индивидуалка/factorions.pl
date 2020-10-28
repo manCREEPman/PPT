bc164eab7b22de3d574ddf55de9d661187b49589
manCREEPman <barbekyan@yandex.ru>
2020-10-28 17:25:35 +0300

    Первая индивидуалка закончена

--- 1 индивидуалка/factorions.pl
+++ 1 индивидуалка/factorions.pl
@@ -1,7 +1,7 @@
 % 2540160 - верхняя граница числа, когда сумма факториалов цифр будет
 % меньше самого числа
 
-% Функтор, считающий факториал числа положительного числа.
+% Предикат, считающий факториал числа положительного числа.
 fact(N, _):- N < 0, !.
 fact(0, 1).
 fact(1, 1).
@@ -9,10 +9,10 @@ fact(N, X):- fact(N, 1, 1, X).
 fact(N, N, Mul, Mul):-!.
 fact(N, I, Mul, X):- Mul1 is Mul * (I + 1), I1 is I + 1, fact(N, I1, Mul1, X).
 
-% Функтор, считающий сумму факториалов цифр числа.
+% Предикат, считающий сумму факториалов цифр числа.
 % На вход подаётся число, затем последовательно выделяются его цифры,
 % вычисляются их факториалы и суммируются.
-sumEachNumberFact(Value, Res):- Value < 3, Res is 0.
+sumEachNumberFact(Value, Res):- Value < 3, Res is 0, !.
 sumEachNumberFact(Value, Res):- sumEachNumberFact(Value, 0, Res).
 sumEachNumberFact(0, Sum, Sum):-!.
 sumEachNumberFact(Value, Sum, Res):-
@@ -20,24 +20,27 @@ sumEachNumberFact(Value, Sum, Res):-
     fact(CurNum, TempFact),
     Sum1 is Sum + TempFact,
     Value1 is Value div 10,
-    sumEachNumberFact(Value1, Sum1, Res).
+    sumEachNumberFact(Value1, Sum1, Res), !.
 
-
+% Предикат, инкрементирующий число.
 inc(X, Res):- Res is X + 1, !.
 
-
+% Предикат, перебирающий числа от стартового до 2540160 и суммирующий
+% числа, являющиеся факторионами.
 sumOfFactorions(X, Sum):- sumOfFactorions(X, 0, Sum).
-sumOfFactorions(5, Sum, Sum):-!.
+sumOfFactorions(2540160, Sum, Sum):-!.
 sumOfFactorions(X, CurSum, Sum):-
     sumEachNumberFact(X, SumFactX),
     SumFactX = X,
     CurSum1 is CurSum + X,
     inc(X, X1),
-    sumOfFactorions(X1, CurSum1, Sum).
+    sumOfFactorions(X1, CurSum1, Sum), !.
 sumOfFactorions(X, CurSum, Sum):-
     inc(X, X1),
-    sumOfFactorions(X1, CurSum, Sum).
+    sumOfFactorions(X1, CurSum, Sum), !.
 
 
-ind1:- sumOfFactorions(3, Sum).
+ind1:- writeln("Факторион - число, равное сумме факториалов своих цифр."),
+    writeln("Верхняя граница - 2540160"),
+    sumOfFactorions(3, Sum), write(Sum).
 
--- 2 индивидуалка/list_difference.pl
+++ 2 индивидуалка/list_difference.pl
@@ -8,11 +8,14 @@ readList([], 0):- !.
 readList([H|List], N):- read(H), N1 is N-1, readList(List, N1).
 
 % Предикат проверки, повторяется ли элемент в списке
-checkRepeating([_|_], Elem):- checkRepeating([_|_], Elem, 0).
+checkRepeating(List, Elem):- checkRepeating(List, Elem, 0).
 checkRepeating([_|_], _, RepeatingCount):- RepeatingCount > 1, !.
-checkRepeating([], _, 0):- !, fail.
-checkRepeating([Elem|T], Elem, RepeatingCount):- RPC is RepeatingCount + 1,
+checkRepeating([], _, 0):- fail.
+checkRepeating([H|T], Elem, RepeatingCount):-
+    checkRepeating(T, Elem, RepeatingCount).
+checkRepeating([Elem|T], Elem, RepeatingCount):-
+    RPC is RepeatingCount + 1,
     checkRepeating(T, Elem, RPC).
-checkRepeating([_|T], Elem, RepeatingCount):- checkRepeating(T, Elem, RepeatingCount).
+
 
 
