fdcbd262c94474e346b789931d8de59e5e99e483
manCREEPman <barbekyan@yandex.ru>
2020-11-25 16:01:22 +0300

    asdf

---
+++ 3 индивидуалка/count_permutations.pl
@@ -0,0 +1,14 @@
+read_str(A,N,Flag):-get0(X),r_str(X,A,[],N,0,Flag).
+r_str(-1,A,A,N,N,1):-!.
+r_str(10,A,A,N,N,0):-!.
+r_str(X,A,B,N,K,Flag):-K1 is K+1,append(B,[X],B1),get0(X1),r_str(X1,A,B1,N,K1,Flag).
+
+read_list_str(List):-read_str(A,N,Flag),read_list_str([A],List,Flag).
+read_list_str(List,List,1):-!.
+read_list_str(Cur_list,List,0):-
+	read_str(A,N,Flag),append(Cur_list,[A],C_l),read_list_str(C_l,List,Flag).
+
+pr:- see('D:/Андрей/1.txt'), read_list_str(List), seen,
+    write(List).
+
+
